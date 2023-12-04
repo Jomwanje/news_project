a6d62ad641d9c821a8615018951161f984ff4acc
Jomwanje <jomwanjegachui@gmail.com>
2023-12-04 13:50:47 +0300

    eMobilis Project

--- news/settings.py
+++ news/settings.py
@@ -73,12 +73,14 @@ WSGI_APPLICATION = 'news.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'OPTIONS': {
+            'deterministic': False,
+        },
+    },
 }
 
 
